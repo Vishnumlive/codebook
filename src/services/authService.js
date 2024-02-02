119333e942a47507fa9ee9aa09ac7b8d448d14d9
Vishnumlive <vishnum.official@gmail.com>
2024-02-02 18:52:53 +0530

    update

--- src/services/authService.js
+++ src/services/authService.js
@@ -5,6 +5,7 @@ export async function login(authDetails){
         headers : { "content-type" : "application/json"},
         body : JSON.stringify(authDetails)
     }
+    
 
     const response = await fetch(`${process.env.REACT_APP_HOST}/login`, requestOptions);
     
